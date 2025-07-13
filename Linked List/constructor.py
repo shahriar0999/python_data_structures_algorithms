6af1e01c98c711b14529c9559370738ec5200e55
shahriar0999 <shahriar0999@proton.me>
2025-07-14 00:07:00 +0600

    linked list constructor

--- Linked List/constructor.py
+++ Linked List/constructor.py
@@ -2,3 +2,15 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1 
+
+
+my_linked_list = LinkedList(5)
+print(my_linked_list.head.value)  # Should return the head node with value 5

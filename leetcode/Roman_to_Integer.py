9ca3d55b41e843e1b1ff8b458f5e50cc1d8b2e92
shahriar0999 <shahriar0999@proton.me>
2025-09-06 14:17:42 +0600

    add another solution

--- leetcode/Roman_to_Integer.py
+++ leetcode/Roman_to_Integer.py
@@ -20,3 +20,22 @@ class Solution(object):
 
         return ans
     
+# alter-native solution (best than previous)
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        sym_val = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+        ans = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and sym_val[s[i]] < sym_val[s[i+1]]:
+                ans -= sym_val[s[i]]
+            else:
+                ans += sym_val[s[i]]
+
+        return ans
+    
+    

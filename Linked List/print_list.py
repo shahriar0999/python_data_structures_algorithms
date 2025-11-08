f8019a3590b510f3c2fc5587d4e02158446907bc
shahriar0999 <shahriar0999@proton.me>
2025-11-08 13:56:04 +0600

    print list func added inisde LinkedList as method

--- Linked List/print_list.py
+++ Linked List/print_list.py
@@ -1,11 +1,35 @@
-from constructor import LinkedList
+# from constructor import LinkedList
+
+# my_linked_list = LinkedList(5)
+
+# def print_list(linked_list):
+#     temp = linked_list.head
+#     while temp is not None:
+#         print(temp.value)
+#         temp = temp.next
+
+# print_list(my_linked_list)  # Should print 5
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1 
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
 
 my_linked_list = LinkedList(5)
-
-def print_list(linked_list):
-    temp = linked_list.head
-    while temp is not None:
-        print(temp.value)
-        temp = temp.next
-
-print_list(my_linked_list)  # Should print 5
+my_linked_list.print_list() # Should return the head node with value 5
